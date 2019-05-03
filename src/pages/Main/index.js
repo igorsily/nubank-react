2948ecdbf4ca9e21329777a926576b9085d8543c
Igor Sily <igor.estagiario@cbmes.gov.br.local>
2019-05-03 15:55:54 -0300

    Implementando QRCode

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
 import { 
   Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
@@ -13,6 +14,8 @@ export default function Main() {
     <Container>
       <Header />
       <Content>
+
+        <Menu />
         <Card>
           <CardHeader>
             <Icon name="attach-money" size={28} color="#666" />
